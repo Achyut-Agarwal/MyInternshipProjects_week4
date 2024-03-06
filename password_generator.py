ecaa79512ca651ba93cb0d52b530fa1c5d6e9ac8
Achyut-Agarwal <159720918+Achyut-Agarwal@users.noreply.github.com>
2024-03-06 20:00:10 +0530

    password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,17 +1,23 @@
 import random
 import string
-letters = 'abcdefghijklmnopqrstuvwxzyABCDEFGHIJKLMNOPQRSTUVWXZY'
+
+letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 special_characters = "@#$%^&*"
 password_list = []
-user = int(input('Enter the length of password(in numbers): '))
-for i in range(0,user):
-    number = random.randint(0,9)
-    lett = random.choice(letters)
-    char = random.choice(special_characters)
-    password_list.append(lett)
-    password_list.append(str(number))
-    password_list.append(char)
-    
-random.shuffle(password_list)
-password = ''.join(password_list)
-print(password[:user])
+
+times = int(input('Enter the number of times passwords to be generated: '))
+user = int(input('Enter the length of each password (in numbers): '))
+
+for a in range(times):
+    for i in range(0, user):
+        number = random.randint(0, 9)
+        lett = random.choice(letters)
+        char = random.choice(special_characters)
+        password_list.append(lett)
+        password_list.append(str(number))
+        password_list.append(char)
+
+    random.shuffle(password_list)
+    password = ''.join(password_list)
+    print(password[:user])
+    password_list = []
